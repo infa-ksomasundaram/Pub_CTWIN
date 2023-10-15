7fcacd45c071b2cbb60d4aebfbf5037fec4de01e
infa-ksomasundaram <144805074+infa-ksomasundaram@users.noreply.github.com>
2023-10-15 16:09:06 +0530

    Add files via upload

---
+++ Filebeat-DE.ps1
@@ -0,0 +1,17 @@
+﻿#github url to download zip file
+#Assign zip file url to local variable
+
+$Url = "https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-8.10.3-windows-x86_64.zip"
+
+$DownloadZipFile = "C:\CT-Agents\" + $(Split-Path -Path $Url -Leaf)
+
+$ExtractPath = "C:\CT-Agents\Filebeat\"
+
+Invoke-WebRequest -Uri $Url -OutFile $DownloadZipFile
+
+$ExtractShell = New-Object -ComObject Shell.Application 
+
+$ExtractFiles = $ExtractShell.Namespace($DownloadZipFile).Items() 
+
+$ExtractShell.NameSpace($ExtractPath).CopyHere($ExtractFiles) 
+Start-Process $ExtractPath
---
+++ Filebeat-Install.ps1
@@ -0,0 +1,3 @@
+﻿cd "C:\CT-Agents\Filebeat\filebeat-8.10.3-windows-x86_64\" 
+
+.\install-service-filebeat.ps1
