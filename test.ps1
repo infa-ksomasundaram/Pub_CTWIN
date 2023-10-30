519ab4165598900c5e15bdc5a71c05c177be4e97
infa-ksomasundaram <144805074+infa-ksomasundaram@users.noreply.github.com>
2023-10-30 16:36:09 +0530

    test.ps1

--- test.ps1
+++ test.ps1
@@ -1,1 +1,22 @@
+# Check for pending updates and install them immediately as administrator
+$Session = New-Object -ComObject Microsoft.Update.Session
+$Searcher = $Session.CreateUpdateSearcher()
+$Criteria = "IsInstalled=0 and Type='Software'"
+$SearchResult = $Searcher.Search($Criteria)
 
+if ($SearchResult.Updates.Count -eq 0) {
+    Write-Host "No pending updates found."
+} else {
+    Write-Host "Pending updates found. Installing updates..."
+    
+    # Elevate to administrator rights
+    $ps = [System.Diagnostics.Process]::Start("powershell", "-noprofile -command &{Start-Process PowerShell -ArgumentList 'Install-WindowsUpdate -AcceptAll -AutoReboot -ForceReboot -Verbose' -Verb RunAs}")
+    $ps.WaitForExit()
+}
+
+# Check if a reboot is required
+if ((Get-WmiObject -Class Win32_ComputerSystem).RebootPending) {
+    Write-Host "A reboot is required to complete the update installation."
+} else {
+    Write-Host "Updates installed successfully."
+}
